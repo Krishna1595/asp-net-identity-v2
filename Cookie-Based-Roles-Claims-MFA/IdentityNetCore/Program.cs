c7d02a4c9b91d324c05b4dedd674a9c1f3bc52c9
Krishna Teja <teja991002@gmail.com>
2024-06-07 15:47:43 -0700

    Update Program.cs

--- Cookie-Based-Roles-Claims-MFA/IdentityNetCore/Program.cs
+++ Cookie-Based-Roles-Claims-MFA/IdentityNetCore/Program.cs
@@ -7,77 +7,71 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
+    _ = app.UseExceptionHandler("/Error");
+    _ = app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+var connString = app.Configuration["ConnectionStrings:Default"];
+var smtp = app.Configuration.GetSection("Smtp");
+
+ConfigureServices(builder.Services);
+
+app.Run();
+
+
+void ConfigureServices(IServiceCollection services)
+{
+    _ = services.AddDbContext<ApplicationDBContext>(o => o.UseSqlServer(connString));
+    _ = services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDBContext>()
+        .AddDefaultTokenProviders();
+
+    _ = services.Configure<IdentityOptions>(options =>
     {
-        var builder = WebApplication.CreateBuilder(args);
+        options.Password.RequiredLength = 3;
+        options.Password.RequireDigit = true;
+        options.Password.RequireNonAlphanumeric = false;
 
-        // Add services to the container.
-        builder.Services.AddRazorPages();
-        builder.Services.AddControllersWithViews();
+        options.Lockout.MaxFailedAccessAttempts = 3;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
 
-        var app = builder.Build();
+        options.SignIn.RequireConfirmedEmail = false;
+    });
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            _ = app.UseExceptionHandler("/Error");
-            _ = app.UseHsts();
-        }
+    _ = services.ConfigureApplicationCookie(option =>
+    {
+        option.LoginPath = "/Identity/Signin";
+        option.AccessDeniedPath = "/Identity/AccessDenied";
+        option.ExpireTimeSpan = TimeSpan.FromHours(10);
+    });
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
+    _ = services.Configure<SmtpOptions>(smtp);
 
-        app.UseAuthorization();
-        app.UseAuthentication();
+    _ = services.AddSingleton<IEmailSender, SmtpEmailSender>();
+    _ = services.AddAuthorization(option =>
+    {
+        option.AddPolicy("MemberDep", p => { _ = p.RequireClaim("Department", "Tech").RequireRole("Member"); });
 
-        app.MapDefaultControllerRoute();
-        app.MapRazorPages();
-
-        var connString = app.Configuration["ConnectionStrings:Default"];
-        var smtp = app.Configuration.GetSection("Smtp");
-
-        ConfigureServices(builder.Services);
-
-        app.Run();
-
-
-        void ConfigureServices(IServiceCollection services)
-        {
-            _ = services.AddDbContext<ApplicationDBContext>(o => o.UseSqlServer(connString));
-            _ = services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDBContext>()
-                .AddDefaultTokenProviders();
-
-            _ = services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequiredLength = 3;
-                options.Password.RequireDigit = true;
-                options.Password.RequireNonAlphanumeric = false;
-
-                options.Lockout.MaxFailedAccessAttempts = 3;
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
-
-                options.SignIn.RequireConfirmedEmail = false;
-            });
-
-            _ = services.ConfigureApplicationCookie(option =>
-            {
-                option.LoginPath = "/Identity/Signin";
-                option.AccessDeniedPath = "/Identity/AccessDenied";
-                option.ExpireTimeSpan = TimeSpan.FromHours(10);
-            });
-
-            _ = services.Configure<SmtpOptions>(smtp);
-
-            _ = services.AddSingleton<IEmailSender, SmtpEmailSender>();
-            _ = services.AddAuthorization(option =>
-            {
-                option.AddPolicy("MemberDep", p => { _ = p.RequireClaim("Department", "Tech").RequireRole("Member"); });
-
-                option.AddPolicy("AdminDep", p => { _ = p.RequireClaim("Department", "Tech").RequireRole("Admin"); });
-            });
-            _ = services.AddControllersWithViews();
-        }
-    }
+        option.AddPolicy("AdminDep", p => { _ = p.RequireClaim("Department", "Tech").RequireRole("Admin"); });
+    });
+    _ = services.AddControllersWithViews();
 }
